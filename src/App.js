84b2c2b3daba16805660d215f693f59dbb9fdba7
aedorado <annurag94@gmail.com>
2021-08-17 21:26:50 +0530

    First commimt for NS Class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,158 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 1. Example: Show a user greeting based on whether user is logged in or not
+// 2. Create a mailbox header showing how many unread messages the user has?
+// 3. Toggle a warning banner upon button click! (component returns null)
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: true
+    }
+  }
+
+  toggle = () => {
+    this.setState((state) => ({
+      isOn: !state.isOn
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggle}>
+          { this.state.isOn ? 'ON' : 'OFF' }
+        </button>
+      </div>
+    )
+  }
+
 }
+
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     }
+//     // this.inc = this.inc.bind(this);
+//     // this.dec = this.dec.bind(this);
+//   }
+
+//   inc = (event) => {
+//     console.log(event);
+//     this.setState({
+//       count: this.state.count + 1
+//     });
+//   }
+
+//   dec() {
+//     this.setState((state) => ({
+//       count: state.count - 1
+//     }));
+//   }
+
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <button onClick={this.inc}>Add</button>
+//         <br />
+//         <p>{this.state.count}</p>
+//         <br />
+//         <button onClick={this.dec}>Subtract</button>
+
+//       </div>
+//     )
+//   }
+
+// }
+
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {count: 0}
+//   }
+
+//   inc = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   }
+
+//   dec = () => {
+//     this.setState({
+//       count: this.state.count - 1
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <button onClick={this.inc}> + </button>
+//         <span className="count-display">Count {this.state.count}</span>
+//         <button onClick={this.dec}> - </button>
+//       </>
+//     )
+//   }
+
+// }
+
+// function App(props) {
+//   return (
+//     <div className="App">
+//         <p>This para uses props: {props.name}</p>
+//         <p>Age: {props.age}</p>
+//       </div>
+//   );
+// }
+
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     console.log(props);
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <p>This para uses props: {this.props.name}</p>
+//         <p>Age: {this.props.age}</p>
+//       </div>
+//     );    
+//   }
+  
+// }
+
+
+
+  // // f1 -> f2
+
+  // // this.props.increment = 5
+  // // this.state.counter = 10
+
+  // // 10 - f1 - 15 - f2 - 20
+  // // 10 - f1, f2 - 20
+
+  // f1() {
+  //   this.setState({
+  //     counter: this.state.counter + this.props.increment,
+  //   });
+  //   // this.setState({ counter: 10 + 5 })
+  // }
+
+  // f2() {
+  //   this.setState({
+  //     counter: this.state.counter + this.props.increment,
+  //   });
+  //   // this.setState({ counter: 10 + 5 })
+  // }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,52 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <App></App>
+  </>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000);
+
+// let name = 'React JS !';
+// function gn() {
+//   return name;
+// }
+
+// ReactDOM.render(
+//   // React.createElement(
+//   //   'p', 
+//   //   { className: 'li-class', style: {color: 'blue', fontSize: '24px'} }, 
+//   //   React.createElement(
+//   //     'ul',
+//   //     null, 
+//   //     React.createElement('li', null, "React"),
+//   //     React.createElement('li', null, "Angular"),
+//   //     React.createElement('li', {style:{color:'yellow'}}, "Vue"),
+//   //     React.createElement('li', null, "Express"),
+//   //   )
+//   // ),
+//   // <App />,
+//   <p className="li-class" style={{color: 'blue', fontSize: '24px'}}>
+//     <ul>
+//       <li>{ gn() }</li>
+//       <li>Angular</li>
+//       <li style={{color:'yellow'}}>Vue</li>
+//       <li>Express</li>
+//     </ul>
+//   </p>,
+//   document.getElementById('root')
+// );
